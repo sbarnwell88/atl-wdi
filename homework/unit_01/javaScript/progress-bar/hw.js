27495c65402f360ffd5b1bba2737227c86822eae
sbarnwell88 <susy6688@gmail.com>
2017-07-06 19:02:06 -0400

    finished hw

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -4,15 +4,28 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
+    document.getElementById('numeric-display').innerHTML = timerValue;
     // Your Code Here
   },
   drawProgressBars: function(timerValue){
+    var timeElapsed = 100 - timerValue;
+    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
     // Your Code Here
   },
   drawLitFuses: function(timerValue){
+    var unlitFuses = timerValue / 100;
+    document.getElementsByClassName('unburnt')[0].style.width = unlitFuses * 98 + '%';
+    document.getElementsByClassName('burnt')[0].style.width = (1 - unlitFuses) * 98 + '%';
     // Your Code Here
   },
   drawCrawlers: function(timerValue){
+    var timePassed = 100 - timerValue;
+    if (timePassed % 2 === 0) {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '0px';
+    } else {
+      document.getElementsByClassName('crawler')[0].style.marginTop =  '10px';
+    }
+    document.getElementsByClassName('crawler')[0].style.marginLeft = (timePassed * 10) + 'px';
     // Your Code Here
   }
 };
