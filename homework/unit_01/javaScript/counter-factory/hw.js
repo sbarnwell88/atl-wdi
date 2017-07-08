032268ffc21cb07ec93c35a5a649a161b3177ebe
sbarnwell88 <susy6688@gmail.com>
2017-07-08 15:36:10 -0400

    finished homework

--- homework/unit_01/javaScript/counter-factory/hw.js
+++ homework/unit_01/javaScript/counter-factory/hw.js
@@ -49,15 +49,35 @@ const CounterCollection = {
 const Presenter = {
   insertCounterComponent: function(newCountId){
     console.log(`insert counter component #${newCountId}`);
+    let newCounterComponent = document.createElement('div');
+    newCounterComponent.innerHTML =
+    `<h3>Count: <span>0</span></h3> <button class='increment'> +1 </button> <button class='delete'> Delete </button>`;
+    newCounterComponent.className += ' counter';
+    newCounterComponent.dataset.countId = newCountId;
+    newCounterComponent.getElementsByClassName('increment')[0].onclick= AppController.onClickIncrement;
+    newCounterComponent.getElementsByClassName('delete')[0].onclick = AppController.onClickDelete;
+    document.getElementById('counter-list').appendChild(newCounterComponent);
+
     // Your Code Here
   },
   refreshCounterComponent: function(countId){
     console.log(`refresh counter component #${countId}`);
+    // find counter with 'counterId'
+    let val = CounterCollection.getCounterValue(countId);
+    document.querySelector(`[data-count-id="${countId}"] span`).innerHTML = val;
+    //find value shown in div
+    // find the current count value
+    // set the current count value (div) equal to the counter current value
     // Your Code Here
   },
   removeCounterComponent: function(countId){             // REACH
     console.log(`remove counter component #${countId}`);
     // Your Code Here
+    let counterComponent = document.querySelector(`[data-count-id="${countId}"]`)
+    console.log(counterComponent, counterComponent.parent);
+    counterComponent.parentNode.removeChild(counterComponent);
+    // find counter 'count id' and set it to a variable
+    // remove that specific component from the DOM
   }
 };
 
@@ -65,12 +85,35 @@ const Presenter = {
 const AppController = {
   onClickNewCounter: function(event){
     // Your Code Here
+    CounterCollection.createCounter();
+    Presenter.insertCounterComponent(CounterCollection.lastCountId);
+    console.log(`click new counter (#${CounterCollection.lastCountId})`);
+
+    // locate 
+    //create new component = new counter
+
   },
   onClickIncrement: function(event){
     // Your Code Here
+    // which counter the IU = to
+    let countId = Number(event.target.parentNode.dataset.countId);
+    console.log(`click increment #${countId}`);
+    CounterCollection.incrementCounter(countId);
+    Presenter.refreshCounterComponent(countId);
+
+    // increment the value of the corresponding counter
+    //update visible count value inside IU 
   },
-  onClickDelete: function(event){                           // REACH
+  onClickDelete: function(event){ 
+    let countId = Number(event.target.parentNode.dataset.countId);
+    console.log(`click delete #${countId}`);
+    CounterCollection.incrementCounter(countId);
+    Presenter.removeCounterComponent(countId);                          // REACH
     // Your Code Here
+    // which counter the IU == to
+    // destroy correspongding counter without affecting others
+    // destroy counter
+    //removes IU component from DOM
   }
 };
 
