f5011fd52f4329487f08ef38345b9d603f8d09f7
sbarnwell88 <susy6688@gmail.com>
2017-07-24 17:53:40 -0400

    part 4 added new methods

--- homework/unit_02/javaScript/js-constructor/player.js
+++ homework/unit_02/javaScript/js-constructor/player.js
@@ -15,3 +15,7 @@ var player = {
     console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
   }
 }; //end player
+
+player.feedTamagotchi(firstOne);
+player.medicateTamagotchi(secondOne);
+player.knockOutTamagotchi(thirdOne);
