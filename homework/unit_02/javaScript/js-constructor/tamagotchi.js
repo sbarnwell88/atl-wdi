7dea22655a43974ed392ce6dc03cd36bec7d118b
sbarnwell88 <susy6688@gmail.com>
2017-07-24 17:35:23 -0400

    part 1: created Tamagotchi constructor

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,27 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-
+class Tamagotchi {
+    constructor() {
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+    }
+    cry() {
+        this.foodInTummy -= 1;
+        console.log(this.foodInTummy);
+        console.log('WAHHHH!!!');
+    }
+}
 
 //create new Tamagotchis
+const firstOne = new Tamagotchi;
+const secondOne = new Tamagotchi;
 
 
 //test out your Tamagotchies below via console.logs
+console.log(firstOne);
+console.log(secondOne);
+
+firstOne.cry();
+secondOne.cry();
--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -4,19 +4,40 @@ const fs = require('fs');
 
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
+//   let lines = content.split('\n');
 
-  // clean up the array by removing the empty line
-  lines.pop();
+//   // clean up the array by removing the empty line
+//   lines.pop();
 
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
+
+
+const readTheFilePromise = new Promise(function(resolve, reject) {
+  fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+    if (error) {
+      reject(error);
+    }
+    resolve(content);
   });
 });
+readTheFilePromise
+  .then(data => data.split('\n')) 
+  .then(lines => {
+    lines.pop()
+    lines.forEach(function(line) {
+      console.log('Hello, ' + line + '!');
+    })
+  })
+.catch(function(error) {
+  console.log(error);
+})
