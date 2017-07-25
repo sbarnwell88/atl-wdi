b7fa10540710886d7043e36fc789c148ed45c128
sbarnwell88 <susy6688@gmail.com>
2017-07-24 21:46:40 -0400

    completed number 2

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -2,3 +2,26 @@ setTimeout(function() {
     console.log('TIMED OUT!')
 }, 300);
 
+'use strict';
+// var os = require('os'); os.tmpDir = os.tmpdir;
+
+var promise = new Promise(function(fulfill, reject) {
+    var isFulfilled = true;
+    if (isFulfilled) {
+        fulfill('FULFILLED!');
+    } 
+    else {
+        reject('REJECTED!');
+    }
+    setTimeout(function() {
+    }, 300);
+});
+promise
+    .then(function(fromResolve) {
+        console.log(`${fromResolve}`)
+        return "FULFILLED!";
+    })
+    .catch(function(onReject) {
+        console.log('REJECTED!');
+        return "REJECTED!";
+    });
