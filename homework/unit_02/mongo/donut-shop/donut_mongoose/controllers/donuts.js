6ebd607acec3d27493ce935f89d20a9ed87f9290
sbarnwell88 <susy6688@gmail.com>
2017-07-26 23:04:08 -0400

    got most routes to work

--- homework/unit_02/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -34,14 +34,29 @@ router.get('/', (req, res) => {
 // NEW
 //======================
 // Create a GET new route "/new" that renders the new.hbs form
-
+router.get('/new', (req, res) => {
+    res.render('donuts/new');
+});
 
 
 //======================
 // SHOW
 //======================
 // Create a GET show route "/:id" that renders the donut's show page
+router.get('/:id', (req, res) => {
+    const donutIdToSearchDbFor = req.params.id;
 
+    Donut.findById(donutIdToSearchDbFor)
+        .then((donuts) => {
+            res.render(
+                'donuts/show',
+                { donuts: donuts }
+            );
+        })
+        .catch((error) => {
+            console.log(`Error retrieving user with ID of ${userIdToSearchDbFor}`)
+        });
+});
 
 
 
@@ -50,7 +65,22 @@ router.get('/', (req, res) => {
 //======================
 // Create a POST index route "/" that creates a new donut
 // and upon success redirects back to the index page "/"
+router.post('/', (req, res) => {
 
+    const newDonutInfoFromForm = req.body;
+
+    Donut.create(newDonutInfoFromForm)
+        .then((donuts) => {
+            res.render(
+                'donuts/show',
+                { donuts: donuts }
+            )
+        })
+        .catch((error) => {
+            console.log('Error saving new user to database!');
+            console.log(error);
+        });
+})
 
 
 //======================
@@ -58,7 +88,21 @@ router.get('/', (req, res) => {
 //======================
 // Create a GET edit route "/:id/edit" that renders the edit.hbs page and
 // sends that donut's data to it
+router.get('/:id/edit', (req, res) => {
 
+    const donutIdToFind = req.params.id;
+
+    Donut.findById(donutIdToFind)
+        .then((donuts) => {
+            res.render(
+                'donuts/edit',
+                { donuts: donuts }
+            );
+        })
+        .catch((error) => {
+            console.log(`Error rendering edit form for user with ID of ${userIdToFind}`)
+        })
+});
 
 
 //======================
@@ -66,7 +110,29 @@ router.get('/', (req, res) => {
 //======================
 // Create a PUT update route "/:id" that updates the donut and
 // redirects back to the SHOW PAGE (not index)
+router.put('/:id', (req, res) => {
 
+    const donutIdToUpdate = req.params.id;
+    const updatedDonutInfo = req.body;
+
+    Donut.findByIdAndUpdate(
+        donutIdToUpdate,
+        updatedDonutInfo,
+        { new: true } 
+    )
+        .then((donuts) => {
+
+            res.render(
+                'donuts/show',
+                { donuts: donuts }
+            )
+        })
+        .catch((error) => {
+            console.log(`User with ID of ${donut.id} failed to update!`)
+            console.log(error);
+        })
+
+});
 
 
 //======================
@@ -74,7 +140,17 @@ router.get('/', (req, res) => {
 //======================
 // Create a DELETE delete route "/:id" that deletes the donut and
 // redirects back to index page "/"
+router.get('/:id/delete', (req, res) => {
 
+    const donutIdToDelete = req.params.id;
+
+    Donut.findByIdAndRemove(donutIdToDelete)
+        .then(() => {
+            console.log(`Successfully deleted donut with ID ${donutIdToDelete}!`)
+
+            response.redirect('/');
+        })
+});
 
 
 //======================
