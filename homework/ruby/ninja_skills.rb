e3581291b3ba8b94601f6be1a21eab6a217b861d
sbarnwell88 <susy6688@gmail.com>
2017-08-21 17:45:56 -0400

    input skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -28,16 +28,30 @@
 
 # 4. 
 
-christine = "Hi, I'm Christine"
-likesTo = "long walks on the beach, kinda."
+# christine = "Hi, I'm Christine"
+# likesTo = "long walks on the beach, kinda."
 
-tims = "#{christine} and I like #{likesTo}"
-puts tims
+# tims = "#{christine} and I like #{likesTo}"
+# puts tims
 
-g = "2"
-gToNumber = 2
+# g = "2"
+# gToNumber = 2
 
-puts g.to_i + gToNumber
+# puts g.to_i + gToNumber
+
+# 5. 
+
+# def double(number)
+#     puts number * 2
+# end
+# double(6)
+
+puts "What's your name?"
+user_name = gets.chomp
+puts "What do you like to do?"
+user_like = gets.chomp
+
+puts "#{user_name} likes to #{user_like}"
 
 
 
